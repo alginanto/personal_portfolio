2395d664b37953c36ce3a77381d2b3f975ad1b59
Algin-Anto <119573662+Algin-Anto@users.noreply.github.com>
2025-01-21 19:51:04 +0530

    home page completed

--- lib/pages/hero/hero_page.dart
+++ lib/pages/hero/hero_page.dart
@@ -14,29 +14,30 @@ class HeroPage extends StatelessWidget {
         return Container(
           padding: EdgeInsets.symmetric(horizontal: padding),
           child: isMobile
-              ? _buildMobileHeroContent(info)
-              : _buildDesktopHeroContent(info),
+              ? _buildMobileHeroContent(info, constraints)
+              : _buildDesktopHeroContent(info, constraints),
         );
       },
     );
   }
 
-  Widget _buildMobileHeroContent(PersonalInfo info) {
+  Widget _buildMobileHeroContent(
+      PersonalInfo info, BoxConstraints constraints) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         const SizedBox(height: 32),
-        // ClipRRect(
-        //   borderRadius: BorderRadius.circular(10),
-        //   child: Image.asset(
-        //     info.imageUrl,
-        //     height: 200,
-        //     width: 200,
-        //     fit: BoxFit.cover,
-        //   ),
-        // ),
-        const SizedBox(height: 32),
         _buildHeroText(info),
+        const SizedBox(height: 32),
+        ClipRRect(
+          borderRadius: BorderRadius.circular(10),
+          child: Image.asset(
+            info.imageUrl,
+            height: 300,
+            width: 300,
+            fit: BoxFit.cover,
+          ),
+        ),
         const SizedBox(height: 32),
         Wrap(
           spacing: 20,
@@ -50,36 +51,38 @@ class HeroPage extends StatelessWidget {
     );
   }
 
-  Widget _buildDesktopHeroContent(PersonalInfo info) {
-    return Row(
+  Widget _buildDesktopHeroContent(
+      PersonalInfo info, BoxConstraints constraints) {
+    return Wrap(
+      alignment: WrapAlignment.spaceBetween,
       children: [
         Expanded(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              _buildHeroText(info),
+              SizedBox(
+                  width: constraints.maxWidth / 2, child: _buildHeroText(info)),
               const SizedBox(height: 32),
-              Row(
+              Wrap(
+                spacing: 28,
+                runSpacing: 28,
                 children: info.stats
-                    .map((stat) => Padding(
-                          padding: const EdgeInsets.only(right: 48),
-                          child: _buildStat(stat.value, stat.label),
-                        ))
+                    .map((stat) => _buildStat(stat.value, stat.label))
                     .toList(),
               ),
             ],
           ),
         ),
-        // ClipRRect(
-        //   borderRadius: BorderRadius.circular(10),
-        //   child: Image.asset(
-        //     info.imageUrl,
-        //     height: 400,
-        //     width: 400,
-        //     fit: BoxFit.cover,
-        //   ),
-        // ),
+        ClipRRect(
+          borderRadius: BorderRadius.circular(10),
+          child: Image.asset(
+            info.imageUrl,
+            height: 400,
+            width: 400,
+            fit: BoxFit.cover,
+          ),
+        ),
       ],
     );
   }
@@ -115,12 +118,15 @@ class HeroPage extends StatelessWidget {
 
   Widget _buildStat(String value, String label) {
     return Container(
+      width: 150,
+      height: 150,
       padding: const EdgeInsets.all(16),
       decoration: BoxDecoration(
-        color: Colors.grey[800]?.withOpacity(0.3),
+        color: Colors.grey[800],
         borderRadius: BorderRadius.circular(8),
       ),
       child: Column(
+        mainAxisSize: MainAxisSize.min,
         children: [
           Text(
             value,
@@ -132,7 +138,7 @@ class HeroPage extends StatelessWidget {
           const SizedBox(height: 8),
           Text(
             label,
-            style: const TextStyle(color: Colors.grey),
+            style: const TextStyle(color: Colors.grey, fontSize: 18),
             textAlign: TextAlign.center,
           ),
         ],
